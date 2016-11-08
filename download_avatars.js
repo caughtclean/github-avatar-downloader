0ad1350be2bacc2c1fbef7f3c28ed8021c006e96
Nathan Eyerley <eyerleyn@gmail.com>
2016-11-08 05:52:09 +0000

    added finishing touches, and cleaned up the code for submission

--- download_avatars.js
+++ download_avatars.js
@@ -1,24 +1,24 @@
-
 // installed the request files and plug them in here and make a var
 var GITHUB_USER = 'caughtclean';
 var GITHUB_TOKEN = '54b30d40e3b526a09e4ebcad385973156e3ca66c';
 var request = require('request');
 var fs = require('fs');
+// added options to input arguments from node to download any users avatar
 var repoowner = process.argv[2]
 var reponame = process.argv[3]
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 // this function will pull the Repo contributors
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   // added var options to add user agent header and url
   var options = {
-    url: requestURL,
-    headers: {
-      'User-Agent': 'GitHub Avatar Downloader - Student Project',
+      url: requestURL,
+      headers: {
+        'User-Agent': 'GitHub Avatar Downloader - Student Project',
+      }
     }
-  }
-  //
+    // parsing the raw JSON string into a array of objects we can deal with
   request(options, function(err, response, body) {
     if (err) throw err;
     jobj = JSON.parse(body)
@@ -28,25 +28,16 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 };
 
-// vars with my username and passwords to pull API info
 
 
 getRepoContributors(repoowner, reponame, function(results) {
   // lopping through the array of objs to select only the avatar urls
-    results.forEach(function(result) {
+  results.forEach(function(result) {
     downloadImageByURL(result.avatar_url, result.login);
   });
+  // function that intiated request to download a file at a certain path (called on by arguments)
+  function downloadImageByURL(url, filePath) {
+    request(url).pipe(fs.createWriteStream('./avatars/' + filePath + '.jpg'));
+  }
 
-    function downloadImageByURL(url, filePath) {
-      request(url).pipe(fs.createWriteStream('./avatars/' + filePath + '.jpg'));
-}
-
-  // mapped results to a array if needed later
-  // var urls = results.map(function(result) {
-  //   return result.avatar_url;
-  // });
-
-  // console.log('urls', urls);
-
-  // console.log("Result:", jobj[0].avatar_url);
 });
