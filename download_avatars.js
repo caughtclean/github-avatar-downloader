ce00b4a166fa8e43ed5e7c962231a46dc2bce957
Nathan Eyerley <eyerleyn@gmail.com>
2016-11-08 04:37:30 +0000

    updated downloadeder code to loop through the JSON string and return only the avatar_url values

--- download_avatars.js
+++ download_avatars.js
@@ -1,18 +1,44 @@
 
-// installed the request files and plug them in here
+// installed the request files and plug them in here and make a var
+var GITHUB_USER = 'caughtclean';
+var GITHUB_TOKEN = '54b30d40e3b526a09e4ebcad385973156e3ca66c';
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 // this function will pull the Repo contributors
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL)
-}
-// vars with my username and passwords to pull API info
-var GITHUB_USER = "YOUR USERNAME HERE";
-var GITHUB_TOKEN = "YOUR ACCESSTOKEN HERE";
+  // added var options to add user agent header and url
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project',
+    }
+  }
+  //
+  request(options, function(err, response, body) {
+    if (err) throw err;
+    jobj = JSON.parse(body)
+    cb(jobj)
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  });
+
+};
+
+// vars with my username and passwords to pull API info
+
+
+getRepoContributors("jquery", "jquery", function(results) {
+  // lopping through the array of objs to select only the avatar urls
+    results.forEach(function(result) {
+    console.log('result.avatar_url', result.avatar_url);
+  });
+  // mapped results to a array if needed later
+  // var urls = results.map(function(result) {
+  //   return result.avatar_url;
+  // });
+
+  // console.log('urls', urls);
+
+  // console.log("Result:", jobj[0].avatar_url);
 });
