d8905a5b107619c88cd85315fa769eb5b57c7474
Nathan Eyerley <eyerleyn@gmail.com>
2016-11-08 01:35:28 +0000

    added more code to the downloader

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,16 @@
+
+// installed the request files and plug them in here
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
-
+// this function will pull the Repo contributors
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL)
 }
+// vars with my username and passwords to pull API info
+var GITHUB_USER = "YOUR USERNAME HERE";
+var GITHUB_TOKEN = "YOUR ACCESSTOKEN HERE";
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
